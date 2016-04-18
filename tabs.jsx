07ec2d5cabf4fb284479aad021d6c55d768c3812
momajd <mo.a.majd@gmail.com>
2016-04-18 11:27:45 -0700

    adds clickable tabs functionality

---
+++ tabs.jsx
@@ -0,0 +1,44 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Tabs = React.createClass({
+  getInitialState: function(){
+    return { selected: 0 };
+  },
+
+  clicked: function(index){
+    this.setState({selected: index});
+  },
+
+  render: function() {
+    var tabs = this.props.tabs;
+    var self = this;
+
+    return (
+      <div>
+        <ul className="tab-titles">
+          {
+            tabs.map(function(tab, index) {
+
+              var style = "unselected";
+
+              if(self.state.selected === index){
+                style = 'selected';
+              }
+
+              return (
+                <h1 className={ style } onClick={self.clicked.bind(self, index)}>
+                  {tab.title}
+                </h1>);
+            })
+          }
+        </ul>
+        <article>
+          {tabs[self.state.selected].content}
+        </article>
+      </div>
+    );
+  }
+});
+
+module.exports = Tabs;
--- widgets.jsx
+++ widgets.jsx
@@ -1,14 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Tabs = require('./tabs');
 
-var MyComponent = React.createClass({
+var tabContent = [
+              {title: "title1", content: "here is some content 1"},
+              {title: "title2", content: "here is some content 2"},
+              {title: "title3", content: "here is some content 3"},
+              {title: "title4", content: "here is some content 4"},
+              {title: "title5", content: "here is some content 5"},
+              {title: "title6", content: "here is some content 6"},
+              {title: "title7", content: "here is some content 7"},
+            ];
+
+
+var Widgets = React.createClass({
   render: function () {
     return(
-      <div>Hello World</div>
+      <div>
+        <Tabs tabs={ tabContent }/>
+      </div>
     );
   }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<MyComponent />, document.getElementById('main'));
+  ReactDOM.render(<Widgets />, document.getElementById('main'));
 });
